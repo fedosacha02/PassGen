6e62e8fbc3d33b816fab373e36cd6d45026dd6b1
Oleksandr Fedorov <fedorov.oleksandr@polytech.pp.ua>
2026-06-07 23:52:46 +0300

    Fixed HTTP responses

--- Backend/HTTP/server.cpp
+++ Backend/HTTP/server.cpp
@@ -9,6 +9,7 @@
 #include <cstring>
 #include <sys/wait.h>
 #include <signal.h>
+#include <sys/stat.h> // get a file size
 
 #include "../Functions/functions.cpp" // compare_strings
 
@@ -16,6 +17,12 @@
 #define BACKLOG 1   // how many pending connections queue will hold
 #define BUFFER_SIZE 1024
 
+/*HTTP::HTTPResponse(char status_code[4], char content_length[10], char content_type[10], char* body){
+    assign(status_code, this->status_code);
+    assign(content_lenght, this->content_length);
+    assign(content_type, this->content_type);
+    assign(file_path, this->file_path);
+};*/
 
 void sigchld_handler(int s)
 {
@@ -40,26 +47,33 @@ void *get_in_addr(struct sockaddr *sa)
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-void send_html(int client_socket, const char* file_path) {
-    FILE* html_file = fopen(file_path, "r");
-    if (html_file == nullptr) {
-        perror("Error opening HTML file.");
+void send_file(const int client_socket, const char* file_path, unsigned short status_code, const char* content_type) {
+    FILE* file = fopen(file_path, "rb");
+    if (file == nullptr) {
+        perror("Error opening the file.");
         return;
     }
-
+    // Get a file size by means of reading metadata
+    struct stat st;
+    stat(file_path, &st);
+    int size = st.st_size;
     char buffer[BUFFER_SIZE];
     size_t bytes_read;
-
-    char* http_header = (char*)"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+    char http_header[150];
+    sprintf(http_header, "HTTP/1.1 %d\r\nServer: Custom/1.0\r\nContent-Type: %s; charset=UTF-8\r\nContent-Length: %d\r\nCache-Control: no-store\r\nConnection: close\r\n\r\n", (int)status_code, content_type, size);
+    std::cout << http_header << '\n';
     if(send(client_socket, http_header, strlen(http_header), 0) == -1){
         perror("send: http header");
     }
 
-    while((bytes_read = fread(buffer, 1, BUFFER_SIZE, html_file)) > 0) {
-        if(send(client_socket, buffer, bytes_read, 0) == -1) perror("send: html content");
+    while((bytes_read = fread(buffer, 1, BUFFER_SIZE, file)) > 0) {
+        if(send(client_socket, buffer, bytes_read, 0) == -1) perror("send: file content");
     }
-    std::cout << "The HTML page located at " << file_path << " was successfully sended.\n";  
-    fclose(html_file);
+    std::cout << "The file located at " << file_path << " was successfully sended.\n"; 
+    
+    fclose(file);
+    
+
 }
 
 int get_http_info(char* output, const char* buffer, int displacement = 0){
@@ -93,15 +107,16 @@ void start(){
 
     if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return;
     }
      // loop through all the results and bind to the first we can
-    for(p = servinfo; p != nullptr; p = p->ai_next) {
+    for(p = servinfo; p != NULL; p = p->ai_next) {
         if ((sockfd = socket(p->ai_family, p->ai_socktype,
                 p->ai_protocol)) == -1) {
             perror("server: socket");
             continue;
         }
-
+        
         if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
                 sizeof(int)) == -1) {
             perror("setsockopt");
@@ -135,7 +150,6 @@ void start(){
         perror("sigaction");
         exit(1);
     }
-
     printf("server: waiting for connections...\n");
 
     while(1) {  // main accept() loop
@@ -152,10 +166,12 @@ void start(){
             get_in_addr((struct sockaddr *)&their_addr),
             s, sizeof s);
         printf("server: got connection from %s\n", s);
-
+        
         if (!fork()) { // this is the child process
+            close(sockfd); // child doesn't need the listener
+
             char buffer[1024] = {0};
-            read(new_fd, buffer, sizeof(buffer) - 1);
+            read(new_fd, buffer, sizeof(buffer));
             std::cout << buffer << '\n';
             char method[7] = {};
             int method_length = get_http_info(method, buffer);
@@ -165,21 +181,35 @@ void start(){
             std::cout << "The acquired method is " << method << ' ' << method_length << '\n';
             std::cout << "The required path is " << path << ' ' << path_length << '\n';
             
-            
-            if(compare_strings(method, (const char*)"GET")){
-                
-                if(compare_strings(path, (const char*)"/")){
-                    std::cout << "GET response:\n"; 
-                    send_html(new_fd, "../Layout/markup/login.html");
-                }
-            }
-       
-            //close(sockfd); // child doesn't need the listener
            
-            //close(new_fd);
+            if(compare_strings(method, (const char*)"GET")){
+                //send(new_fd, "HTTP/1.1 101 Switching Protocols\r\nConnection: Upgrade\r\nUpgrade: h2c\r\n\r\n",70, 0);
+                if(compare_strings(path, (const char*)"/")){
+                    std::cout << "Sending login.html...\n"; 
+                    send_file(new_fd, "../Layout/markup/login.html", 200, "text/html");
+                }
+                else if(compare_strings(path, (const char*)"/styles/styles.css")){
+                    std::cout << "Sending styles.css...\n"; 
+                    send_file(new_fd, "../Layout/styles/styles.css", 200, "text/css");
+                }
+                else if(compare_strings(path, (const char*)"/media/logotype.svg")){
+                    std::cout << "Sending logotype.svg...\n"; 
+                    send_file(new_fd, "../Layout/media/logotype.svg", 200, "image/svg+xml");
+                }
+                else if(compare_strings(path, (const char*)"/styles/not_found.css")){
+                    std::cout << "Sending not_found.css...\n"; 
+                    send_file(new_fd, "../Layout/styles/not_found.css", 200, "text/css");
+                }
+                else{
+                    std::cout << "Sending not_found.html...\n"; ;
+                    send_file(new_fd, "../Layout/markup/not_found.html", 404, "text/html");
+                }
+                
+            }
+    
+            close(new_fd);
+            exit(0);
         }
-        
-        
-        //close(new_fd);  // parent doesn't need this
+        close(new_fd);  // parent doesn't need this
     }
 }
